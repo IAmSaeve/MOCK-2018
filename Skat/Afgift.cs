9f248dbc7209133b7f97aad93b04c8d4e03da0a4
Sebastian Petersen <sebastian@srmail.dk>
2018-09-28 20:49:30 +0200

    Begin task 1

---
+++ Skat/Afgift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Skat
+{
+    public class Class1
+    {
+        public int BilAfgift(int pris)
+        {
+
+        }
+
+        public int ElBilAfgift(int pris)
+        {
+
+        }
+    }
+}
